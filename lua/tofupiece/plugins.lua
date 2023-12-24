50d51fca30b1c4b6bdc98973407c793816204a96
khoaluu2014 <khoaluu2014@gmail.com>
2023-12-24 15:24:33 -0800

    add noice

--- lua/tofupiece/plugins.lua
+++ lua/tofupiece/plugins.lua
@@ -11,6 +11,22 @@ require("lazy").setup({
     {'nvim-tree/nvim-web-devicons'},
     {'fladson/vim-kitty'},
     {'norcalli/nvim-colorizer.lua'},
+    -- lazy.nvim
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    },
     {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
