0859429cab4351eda4fa9f6d8d87bf06a5c363c6
khoaluu2014 <khoaluu2014@gmail.com>
2023-12-30 23:53:28 -0800

    change indentation character

--- lua/tofupiece/plugins_config/indent-blankline.lua
+++ lua/tofupiece/plugins_config/indent-blankline.lua
@@ -21,4 +21,7 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
 
-require("ibl").setup { indent = { highlight = highlight } }
+vim.g.rainbow_delimiter = { highlight = highlight }
+require("ibl").setup { 
+    indent = { highlight = highlight, char="|" }, 
+}
