5a342be145243b717861d06d3ac951b8a9f548e6
khoaluu2014 <khoaluu2014@gmail.com>
2024-06-30 10:46:48 -0700

    changed color to macchiato

--- lua/tofupiece/plugins_config/colorscheme.lua
+++ lua/tofupiece/plugins_config/colorscheme.lua
@@ -1,6 +1,6 @@
 -- catppuccin
 require("catppuccin").setup({
-    flavour = "frappe",
+    flavour = "macchiato",
     transparent_background = false,
     styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
         comments = { "italic" },
