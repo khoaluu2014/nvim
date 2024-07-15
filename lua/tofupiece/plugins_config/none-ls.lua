037b0c7e34c4b41a4d324dd2047e9bd997e42396
khoaluu2014 <khoaluu2014@gmail.com>
2024-07-14 17:45:36 -0700

    adds prettierd config and installs eslint_d

--- lua/tofupiece/plugins_config/none-ls.lua
+++ lua/tofupiece/plugins_config/none-ls.lua
@@ -6,9 +6,14 @@ local async = event == "BufWritePost"
 
 null_ls.setup({
 	sources = {
-		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.prettierd.with({
+			env = {
+				PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.config/nvim/utils/linter/prettierrc.json"),
+			},
+		}),
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.black,
+		null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.formatting.csharpier,
 	},
 	on_attach = function(client, bufnr)
---
+++ utils/linter/test.js
@@ -0,0 +1,5 @@
+function test() {
+  console.log(
+    'This abasdfjaskfaosjfajsdfajsdlkfajsf should use asdkfjals;dkfja;klsjflasjfla single quotes and be indented with 4 spaces',
+  );
+}
