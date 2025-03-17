4ebac4f3a8ab82332ea5a71c5d25809a57dca009
khoaluu2014 <khoaluu2014@gmail.com>
2025-03-17 12:57:13 -0700

    change dap hotkey

--- lua/tofupiece/plugins_config/dap.lua
+++ lua/tofupiece/plugins_config/dap.lua
@@ -1,6 +1,5 @@
 require("dapui").setup()
 
 local dap = require("dap")
-
-vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
-vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, {})
+vim.keymap.set("n", "<F5>", dap.continue, {})
